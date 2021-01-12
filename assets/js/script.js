9b573a0296b616014e4f7efbb4073669112d0301
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-01-11 21:17:24 -0600

    added save to edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,6 +58,36 @@ $(".list-group").on("click", "p", function() {
 });
 
 
+$(".list-group").on("blur", "textarea", function() {
+  // get the textarea's current value/text
+  var text = $(this)
+  .val()
+    .trim();
+
+  // get the parent ul's id attribute
+  var status = $(this)
+  closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
+
+  // get the task's position in the list of other li elements
+  var index = $(this)
+  .closest(".list-group-item")
+  .index();
+
+  tasks[status][index].text = text;
+  saveTasks();
+
+  // recreate p element
+  var taskP = $("<p>")
+  .addClass("m-1")
+  .text(text);
+
+  // replace textarea with p element
+  $(this).replaceWith(taskP);
+});
+
+
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
