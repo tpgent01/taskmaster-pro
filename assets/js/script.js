6c8491effe063abfdd290621e74944c2dad564f6
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-01-12 16:14:55 -0600

    comvert lists into sortables

--- assets/js/script.js
+++ assets/js/script.js
@@ -173,8 +173,61 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
-// load tasks for the first time
-loadTasks();
+
+// sortable task option
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+  },
+  deactivate: function(event) {
+  },
+  over: function(event) {
+  },
+  out: function(event) {
+  },
+  update: function(event) {
+    // array to store the task data in 
+    var tempArr = []
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      // add task data to the temp array as an object
+      tempArr.push({
+        test: text,
+        date: date
+      });
+    });
+
+    // trim down list's ID to match object property
+    var arrName = $(this)
+    .attr("id")
+    .replace("list-", "");
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+
+    // load tasks for the first time
+    loadTasks();
+
+    console.log(tempArr);
+  }
+});
+
+
 
 
 
