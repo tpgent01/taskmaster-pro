ab50da48c64a5a257c6094ebc5e0d0358a68f0be
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-01-11 21:06:08 -0600

    edit task text added

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,6 +46,16 @@ var saveTasks = function() {
 };
 
 
+$(".list-group").on("click", "p", function() {
+  var text = $(this)
+    .text()
+    .trim();
+  var textInput = $("<textarea>")
+    .addClass("form-control")
+    .val(text);
+  $(this).replaceWith(textInput);
+  textInput.trigger("focus");
+});
 
 
 // modal was triggered
